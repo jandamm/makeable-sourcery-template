fba8c2147f1867162cbf76a7a989d4e4df7d9cae
Jan Dammshäuser <jandammshaeuser@gmail.com>
2018-04-17 12:05:41 +0200

    Remove non static templates

--- Example/Makeable.generated.swift
+++ Example/Makeable.generated.swift
@@ -3,16 +3,41 @@
 
 // swiftlint:disable line_length
 
-func makeFile(name: String) -> (_ fileEnding: String) -> (_ locked: Bool) -> File {
-    return { (fileEnding: String) in { (locked: Bool) in
-        return File(name: name, fileEnding: fileEnding, locked: locked)
-    } }
+extension File {
+    static func make(name: String) -> (_ fileEnding: String) -> (_ locked: Bool) -> (_ owner: User) -> File {
+        return { (fileEnding: String) in { (locked: Bool) in { (owner: User) in
+            return File(name: name, fileEnding: fileEnding, locked: locked, owner: owner)
+        }
+        }
+        }
+    }
 }
 
-func makeUser(name: String) -> (_ id: Int) -> (_ email: String?) -> User {
-    return { (id: Int) in { (email: String?) in
-        return User(name: name, id: id, email: email)
-    } }
+extension GenericTest {
+    static func make(base: A) -> (_ baseTwo: B) -> (_ descripition: String) -> GenericTest {
+        return { (baseTwo: B) in { (descripition: String) in
+            return GenericTest(base: base, baseTwo: baseTwo, descripition: descripition)
+        }
+        }
+    }
+}
+
+extension StaticTest {
+    static func make(name: String) -> (_ variable: String) -> (_ locked: Bool) -> StaticTest {
+        return { (variable: String) in { (locked: Bool) in
+            return StaticTest(name: name, variable: variable, locked: locked)
+        }
+        }
+    }
+}
+
+extension User {
+    static func make(name: String) -> (_ id: Int) -> (_ email: String?) -> User {
+        return { (id: Int) in { (email: String?) in
+            return User(name: name, id: id, email: email)
+        }
+        }
+    }
 }
 
 // swiftlint:enable line_length
--- Example/Makeable.min.generated.swift
+++ Example/Makeable.min.generated.swift
@@ -3,12 +3,28 @@
 
 // swiftlint:disable line_length
 
-func makeFile(name: String) -> (_ fileEnding: String) -> (_ locked: Bool) -> File {
-    return { fileEnding in { locked in File(name: name, fileEnding: fileEnding, locked: locked) } }
+extension File {
+    static func make(name: String) -> (_ fileEnding: String) -> (_ locked: Bool) -> (_ owner: User) -> File {
+        return { fileEnding in { locked in { owner in File(name: name, fileEnding: fileEnding, locked: locked, owner: owner) } } }
+    }
 }
 
-func makeUser(name: String) -> (_ id: Int) -> (_ email: String?) -> User {
-    return { id in { email in User(name: name, id: id, email: email) } }
+extension GenericTest {
+    static func make(base: A) -> (_ baseTwo: B) -> (_ descripition: String) -> GenericTest {
+        return { baseTwo in { descripition in GenericTest(base: base, baseTwo: baseTwo, descripition: descripition) } }
+    }
+}
+
+extension StaticTest {
+    static func make(name: String) -> (_ variable: String) -> (_ locked: Bool) -> StaticTest {
+        return { variable in { locked in StaticTest(name: name, variable: variable, locked: locked) } }
+    }
+}
+
+extension User {
+    static func make(name: String) -> (_ id: Int) -> (_ email: String?) -> User {
+        return { id in { email in User(name: name, id: id, email: email) } }
+    }
 }
 
 // swiftlint:enable line_length
