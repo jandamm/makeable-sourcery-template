d4b34c31db3be341eff548d99e458b827830fc7c
Jan Dammshäuser <jandammshaeuser@gmail.com>
2018-04-17 23:05:47 +0200

    Create more test structs

--- Example/Input/Example.swift
+++ Example/Input/Example.swift
@@ -1,14 +1,13 @@
 
-struct User: Makeable {
+struct User: Makeable, Equatable {
     let name: String
     let id: Int
     let email: String?
 }
 
-struct File: Makeable {
+struct File: Makeable, Equatable {
     let name: String
     let fileEnding: String
     let locked: Bool
     var owner: User
 }
-
--- Example/Input/TestCases.swift
+++ Example/Input/TestCases.swift
@@ -1,12 +1,46 @@
 
-struct GenericTest<A, B>: Makeable {
-	let base: A
-	let baseTwo: B
-	let descripition: String
+struct OnlyOkVariables: Makeable, Equatable {
+    static let name = "name"
+    static var variable = "variable"
+    let ok1: Bool
+    let def = "def"
+    var ok2: Int
+    var ok3 = 1
+    var compValue: String {
+        return "\(ok1) comp"
+    }
 }
 
-struct StaticTest: Makeable {
-   static let name = "name"
-   static var variable = "variable"
-   let locked: Bool
+struct GenericTest<A, B>: Makeable {
+    let base: A
+    let baseTwo: B
+    let descripition: String
+}
+
+extension GenericTest: Equatable where A: Equatable, B: Equatable {
+    static func == (lhs: GenericTest<A, B>, rhs: GenericTest<A, B>) -> Bool {
+        guard lhs.base == rhs.base else { return false }
+        guard lhs.baseTwo == rhs.baseTwo else { return false }
+        guard lhs.descripition == rhs.descripition else { return false }
+        return true
+    }
+}
+
+struct StaticTest: Makeable, Equatable {
+    static let name = "name"
+    static var variable = "variable"
+    let locked: Bool
+}
+
+struct DefaultVariablesTest: Makeable, Equatable {
+    let def = "def"
+    var variable: Int
+}
+
+struct ComputedVariablesTest: Makeable, Equatable {
+    let value: String
+
+    var compValue: String {
+        return "\(value) comp"
+    }
 }
